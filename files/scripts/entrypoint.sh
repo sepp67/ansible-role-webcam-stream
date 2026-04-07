0c7d8f2fb8f9c77857a72117af88ad523f240df1
sébastien clem <sebastien@clem.lavallee.tech>
2026-04-07 09:01:15 +0200

    make ffmpeg script stronger. Deleteing audi and surveillance of the script to restart it

--- files/scripts/entrypoint.sh
+++ files/scripts/entrypoint.sh
@@ -14,24 +14,38 @@ EOF
 
 chmod 0644 /etc/cron.d/webcam-snapshots
 crontab /etc/cron.d/webcam-snapshots
-
 service cron start
 
-ffmpeg \
-  -rtsp_transport tcp \
-  -i "${RTSP_URL}" \
-  -c:v copy \
-  -c:a aac \
-  -f hls \
-  -hls_time "${HLS_TIME:-2}" \
-  -hls_list_size "${HLS_LIST_SIZE:-5}" \
-  -hls_flags delete_segments+append_list+omit_endlist \
-  -hls_delete_threshold 1 \
-  /var/www/html/stream/index.m3u8 >/var/log/ffmpeg-hls.log 2>&1 &
+run_ffmpeg() {
+  while true; do
+    echo "$(date -Is) starting ffmpeg" >> /var/log/ffmpeg-hls.log
+
+    ffmpeg \
+      -hide_banner \
+      -loglevel info \
+      -rtsp_transport tcp \
+      -fflags +genpts+discardcorrupt \
+      -use_wallclock_as_timestamps 1 \
+      -i "${RTSP_URL}" \
+      -an \
+      -c:v copy \
+      -f hls \
+      -hls_time "${HLS_TIME:-2}" \
+      -hls_list_size "${HLS_LIST_SIZE:-5}" \
+      -hls_flags delete_segments+append_list+omit_endlist+independent_segments \
+      -hls_delete_threshold 1 \
+      /var/www/html/stream/index.m3u8 >> /var/log/ffmpeg-hls.log 2>&1
+
+    rc=$?
+    echo "$(date -Is) ffmpeg exited rc=${rc}, restart in 3s" >> /var/log/ffmpeg-hls.log
+    sleep 3
+  done
+}
+
+run_ffmpeg &
 
 sleep 2
-cat /var/log/ffmpeg-hls.log || true
+tail -n 50 /var/log/ffmpeg-hls.log || true
 
 nginx -t
-
 exec nginx -g "daemon off;"
